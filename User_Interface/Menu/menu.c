6f1c56a95cce6d667064864aa4f42c9c25f7d5bb
hagarhasheshh <hagarhashesh@std.mans.edu.eg>
2023-05-12 12:33:08 +0300

    menu

---
+++ User_Interface/Menu/menu.c
@@ -0,0 +1,11 @@
+#include "menu.h"
+
+#include <stdio.h>
+
+/*Implement a menu to display the main menu to the user
+ * Allow the user to select the desired mode */
+
+int main()
+{
+    return 0 ;
+}
