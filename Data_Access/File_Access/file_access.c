23ef7e4f07c2d7b528b02c45ef5db36566f74125
hagarhasheshh <hagarhashesh@std.mans.edu.eg>
2023-05-12 12:31:23 +0300

    file access

---
+++ Data_Access/File_Access/file_access.c
@@ -0,0 +1,9 @@
+#include "file_access.h"
+
+/* Add student record
+   Remove student record
+   View student record
+   View all records
+   Edit admin password
+   Create csv file to store student records
+   Create csv file to store admin password */
