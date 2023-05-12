68ee8d8f10e643c42ead25b59fd0923cf2791620
hagarhasheshh <hagarhashesh@std.mans.edu.eg>
2023-05-12 12:32:11 +0300

    memory management

---
+++ Data_Access/Memory_Management/memory_management.c
@@ -0,0 +1,4 @@
+#include "memory_management.h"
+
+/* Implement dynamic memory allocation to store student names and passwords (allocate memory)
+ * Take care of hidden memory leaks when removing or editing student records (free memory) */
