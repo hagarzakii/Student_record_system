0b82c307c1a9f01a3e26ecaa7f2743ad2869d2fc
Mostafa AboSalama <mostafaabosalama661@gmail.com>
2023-05-13 17:19:53 +0200

    Edit Admin Password

--- Business_Logic/Admin/admin.c
+++ Business_Logic/Admin/admin.c
@@ -1,3 +1,12 @@
 #include "admin.h"
 
-// Change the admin password
+char admin_password[50] = "1234"; // default admin password "1234"
+
+void change_admin_password(char current_password[], char new_password[]) {
+    if (strcmp(current_password, admin_password) != 0) {
+        printf("Incorrect current password. Password not changed.\n");
+        return;
+    }
+    strcpy(admin_password, new_password);
+    printf("Admin password changed successfully!\n");
+}
--- Business_Logic/Admin/admin.h
+++ Business_Logic/Admin/admin.h
@@ -1,4 +1,16 @@
 #ifndef STUDENTRECORDSYSTEM_ADMIN_H
 #define STUDENTRECORDSYSTEM_ADMIN_H
 
+#include <stdio.h>
+#include <string.h>
+
+struct node {
+    // Define the node structure here
+};
+
+extern char admin_password[50];
+
+void change_admin_password(char current_password[], char new_password[]);
+
+
 #endif //STUDENTRECORDSYSTEM_ADMIN_H
