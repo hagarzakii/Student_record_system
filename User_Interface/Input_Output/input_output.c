4b4b06823c1922464468169ae3081fbe8c645a6a
hagarhasheshh <hagarhashesh@std.mans.edu.eg>
2023-05-15 20:41:36 +0300

    added function to view all students records

--- User_Interface/Input_Output/input_output.c
+++ User_Interface/Input_Output/input_output.c
@@ -1,3 +1,15 @@
 #include "input_output.h"
 
 // Implement standard input/output functions to interact with the user
+void view_all_records() {
+  //we need counter for number of students
+printf("\nAll Student Records:\n");
+for (int i = 0; i < numStudents; i++) {
+    printf("Student %d:\n", i + 1);
+    printf("First Name: %s\n", st[i]->firstName);
+    printf("Last Name: %s\n", st[i]->lastName);
+    printf("ID: %d\n", st[i]->id_number);
+    printf("GPA: %f\n", st[i]->GPA);
+    printf("Phone Number: %d\n", st[i]->phone_number);
+}
+}
