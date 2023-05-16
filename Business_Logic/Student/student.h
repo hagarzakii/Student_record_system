994c320c6d4c685c8bfcbcd42926c7301980560c
amreziza <132739711+amreziza@users.noreply.github.com>
2023-05-16 18:29:16 +0300

    Update student.h

--- Business_Logic/Student/student.h
+++ Business_Logic/Student/student.h
@@ -1,4 +1,16 @@
 #ifndef STUDENTRECORDSYSTEM_STUDENT_H
 #define STUDENTRECORDSYSTEM_STUDENT_H
+void addstudent();
+void delete_student();
+void edit_student();
+void id_number();
+ struct student
+{
+    char firstName[20];
+   char lastName[20];
+   int id_number;
+   float GPA;
+   int phone_number;
+} st[500];
 
 #endif //STUDENTRECORDSYSTEM_STUDENT_H
