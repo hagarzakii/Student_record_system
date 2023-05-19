c84a7be4478696eaff690102d1a9ac8964688e91
hagarhasheshh <hagarhashesh@std.mans.edu.eg>
2023-05-19 21:50:13 +0300

    called the functions for auth and i/o

--- User_Interface/Menu/menu.c
+++ User_Interface/Menu/menu.c
@@ -1,5 +1,7 @@
 #include "menu.h"
 #include <stdio.h>
+#include "authentication.h"
+#include "input_output.h"
 int main(){
 int na;
 int a=0;  int t;
@@ -10,19 +12,14 @@ printf("Please select number to choise you are admin or student\n"
 
 switch(na){
    case 1:  printf("Welcome in admin mode !\n");
-    printf("please put your user_name :\n");
-    scanf("%s",st[a].user_name);
-    printf("please put your password :\n");
-    scanf("%d",&st[a].pass);
+            is_admin();
+			chooseAdminTask();
    //check admin function   
     break;
 
       case 2:
-     printf("Enter the task that you want to perform\n");
-      printf("1. Find the details of a student using ID number\n");
-      printf("2. Update the details of an student\n");
-      printf("3. Exit\n");
-      scanf("%d", &t);
+	  check_user();
+	  chooseStudentTask();
      // check student function
       break;
 
