5fffe153325c5a48ff57627e0816ff0343eaef09
amreziza <132739711+amreziza@users.noreply.github.com>
2023-05-19 16:23:46 +0300

    Update functions admin.c

--- Business_Logic/Admin/admin.c
+++ Business_Logic/Admin/admin.c
@@ -2,6 +2,97 @@
 
 char admin_password[30] = "1234";  // default admin password "1234"
 
+void addstudent()
+{
+   printf("Add the student's details\n\n");
+   printf("Enter the first name of the student\n");
+   scanf("%s", st[i].firstName);
+   printf("Enter the last name of the student\n");
+   scanf("%s", st[i].lastName);
+   printf("Enter the ID number of the student\n");
+   scanf("%d", &st[i].id_number);
+   printf("Enter the GPA of the student\n");
+   scanf("%f", &st[i].GPA);
+   printf("Enter phone number of the student\n");
+   scanf("%d", &st[i].phone_number);
+   printf("Records saved successfully");
+}
+void delete_student()
+{
+   int temp;
+   printf("Enter the ID number of the student\n");
+   scanf("%d", &temp);
+   for (int j = 1; j <= i; j++)
+   {
+      if (temp == st[j].id_number)
+      {
+         for (int k = j; k < 499; k++)
+         {
+            st[k] = st[k + 1];
+         }
+        i--;
+      }
+   }
+   printf("The entered student's records deleted successfully");
+}
+void edit_student()
+{
+   int temp;
+   printf("Enter the ID number of the student\n");
+   scanf("%d", &temp);
+   for (int j = 0; j < i; j++)
+   {
+      if (temp == st[j].id_number)
+      {
+         printf("1. First Name\n"
+                "2. Last Name\n"
+                "3. ID Number\n"
+                "4. GPA\n"
+                "5.phone number\n");
+         int c;
+         scanf("%d", &c);
+         switch(c) {
+            case 1:
+               printf("Enter the updated first name : ");
+               scanf("%s", st[j].firstName);
+               break;
+            case 2:
+               printf("Enter the updated last name : ");
+               scanf("%s", st[j].lastName);
+               break;
+            case 3:
+               printf("Enter the updated ID number : ");
+               scanf("%d", st[j].id_number);
+               break;
+            case 4:
+               printf("Enter the updated GPA : ");
+               scanf("%f", st[j].GPA);
+               break;
+            case 5:
+               printf("Enter the updated phone number : ");
+               scanf("%d", st[j].phone_number);
+               break;
+         }
+         printf("Records updated successfully");
+      }
+   }
+}
+void id_number()
+{
+   int temp;
+   printf("Enter the ID number of the student\n");
+   scanf("%d", &temp);
+      if (temp == st[i].id_number)
+      {
+         printf("The student's details are\n");
+         printf("The first name is %s\n", st[i].firstName);
+         printf("The last name is %s\n", st[i].lastName);
+         printf("The GPA is %f\n", st[i].GPA);
+         printf("The phone number is %d\n", st[i].phone_number);
+      }
+ }
+
+
 void view_all_records() {
 printf("\nAll Student Records:\n");
 for (int i = 0; i < numstudents; i++) {
