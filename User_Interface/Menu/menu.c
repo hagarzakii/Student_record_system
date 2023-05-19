75dcd87ef65df3adaead8f76a6bcc3d8c06a7e42
amreziza <132739711+amreziza@users.noreply.github.com>
2023-05-19 17:12:52 +0300

    Update menu.c

--- User_Interface/Menu/menu.c
+++ User_Interface/Menu/menu.c
@@ -1,13 +1,14 @@
 #include "menu.h"
 #include <stdio.h>
 int main(){
-int n;
+int na;
 int a=0;  int t;
-printf("Please select number to choise you are admin or student\n" "1: for admin\n"
+printf("Please select number to choise you are admin or student\n" 
+       "1: for admin\n"
        "2: for student\n");
-          scanf("%d",&n);
+          scanf("%d",&na);
 
-switch(n){
+switch(na){
    case 1:  printf("Welcome in admin mode !\n");
     printf("please put your user_name :\n");
     scanf("%s",st[a].user_name);
@@ -15,13 +16,11 @@ switch(n){
     scanf("%d",&st[a].pass);
     break;
 
- case 2:
+      case 2:
      printf("Enter the task that you want to perform\n");
-      printf("1. Add a new student detail\n");
-      printf("2. Find the details of a student using ID number\n");
-      printf("3. Delete the details of an student\n");
-      printf("4. Update the details of an student\n");
-      printf("5. Exit\n");
+      printf("1. Find the details of a student using ID number\n");
+      printf("2. Update the details of an student\n");
+      printf("3. Exit\n");
       scanf("%d", &t);
       break;
 
